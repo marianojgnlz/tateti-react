63ba63451731260361de4c7212f136054fc1d8ee
Mariano Gonzalez <mariano.gonzalez@urbetrack.com>
2023-12-25 17:45:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+};
+
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+  const handleClick = () => {
+    updateBoard(index);
+  }
+
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const updateBoard = (index) => {
+    const newBoard = [ ...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  }
+
+  return (
+    <main className="board">
+      <h1>TaTeTi</h1>
+      <section className="game">
+        {
+          board.map((_, i) => {
+            return (
+              <Square
+                key={i}
+                index={i}
+                updateBoard={updateBoard}
+              >
+                {board[i]}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
+  )
+}
+
+export default App
